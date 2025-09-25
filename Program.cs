dfb7712bbeb6c218c4747f2cdafef0f4d6f96f76
khadijakamran1326 <khadijakamran746@gmail.com>
2025-09-25 12:14:24 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+
+class program 
+{
+  static void Main()
+    {
+        Console.Write(" enter your name: ");
+        string username = Console.ReadLine();
+
+        for (int i = 5; i >= 0; i--)
+        {
+            Console.WriteLine(i);
+
+            if (i == 5)
+            {
+               
+                Thread.Sleep(500);
+            }
+            else if (i > 0)
+            {
+               
+                Thread.Sleep(1000);
+            }
+           
+        }
+
+      
+        Console.WriteLine($"Welcome {username}!");
+    }
+}
